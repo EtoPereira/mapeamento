4ad7fa24744a1215cc2fea53a6a6e434e595a3be
EtoPereira <cienciadacompifrs@gmail.com>
2020-07-03 12:06:03 -0300

    Atualização

--- src/Pages/Inicio/index.js
+++ src/Pages/Inicio/index.js
@@ -3,8 +3,10 @@ import React, { useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Spinner from 'react-bootstrap/Spinner';
 
-import covidImage from '../../image/covid.jpg'
+// import covidImage from '../../image/covid.jpg'
+import banner from '../../image/banner.jpg'
 import city from '../../image/icons/casa.svg'
 import population from '../../image/icons/grupo.svg'
 import confirm from '../../image/icons/confirme.svg'
@@ -12,7 +14,7 @@ import deaths from '../../image/icons/bacterias.svg'
 
 import map from '../../image/mapa.jpeg'
 
-import {DebounceInput} from 'react-debounce-input';
+import { DebounceInput } from 'react-debounce-input';
 
 import moment from 'moment'
 
@@ -22,8 +24,10 @@ import axios from 'axios';
 export default function Inicio() {
 
     const [dados, setDados] = useState(false)
-    const [busca, setBusca] = useState('')
-    let getData = async () => {
+    const [busca, setBusca] = useState('Porto Alegre')
+    const [loading, setLoading] = useState(true)
+
+    let getData = (async () => {
         let res = await axios.get('https://brasil.io/api/dataset/covid19/caso/data?', {
             params: {
                 is_Last: 'True',
@@ -31,39 +35,71 @@ export default function Inicio() {
             }
         });
 
-
         setDados(res.data.results[0])
 
 
-    }
+    })
 
     // const cidade = dados[0]['city']
     // console.log(cidade);
 
 
     useEffect(() => {
-        getData()
+        getData();
+        // handleChange();
     })
 
-    function handleChange(event){
+    function handleChange(event) {
+        if (event.target.value === '') {
+            event.target.value = "Porto Alegre"
+
+            // event.target.value = "São Paulo"
+        }
+
+        console.log(event.target.value);
+
+        event.target.value = event.target.value.toLowerCase()
+            .split(' ')
+            .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(' ');
+
+        console.log(event.target.value);
+
+
         setBusca(event.target.value)
 
+        event.target.value === "" ? setLoading(true) : setLoading(false);
+
+
+
         console.log('Executado');
-        
+
     }
     if (dados === false) {
         return (
-            <div>Carregando dados...</div>
+            <div>
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner><br />
+                <div>
+
+                </div>
+            </div>
         )
     } else {
         return (
             <div className="inicio-container">
                 <header>
-                    <img src={covidImage} alt="Papel de parede do covid"></img>
+                    {/* <h1>INFORMATIVO CORONA VÍRUS</h1> */}
+                    <img src={banner} alt="Papel de parede do covid"></img>
+
+
+                    {/* <img src={covidImage} alt="Papel de parede do covid"></img> */}
                 </header>
                 <InputGroup.Prepend>
                     {/* <InputGroup.Text><FaSearch></FaSearch></InputGroup.Text> */}
-                    <DebounceInput placeholder="Pesquisar cidade" className="input" minLength={2} debounceTimeout={1000} onChange={handleChange}></DebounceInput>
+                    <DebounceInput id="input" placeholder="Pesquisar cidade" className="input" minLength={2} debounceTimeout={1000} onChange={handleChange}></DebounceInput>
+                    {/* {document.getElementById("input") ? <Spinner className="search-spinner" animation="border" size="sm"></Spinner> : <div></div>} */}
                     {/* <input type="text" placeholder="Pesquisar cidade" value={busca} onChange={handleChange}></input> */}
                     {/* <Button onClick={handleChange}>Buscar</Button> */}
                 </InputGroup.Prepend>
@@ -78,13 +114,13 @@ export default function Inicio() {
                 </Card>
                 <CardGroup className="card-group">
 
-                    <Card  className="card">
+                    <Card className="card">
                         <Card.Body>
                             <Card.Title>Cidade</Card.Title>
                             <Card.Text>
                                 {dados.city}, {dados.state}
                             </Card.Text>
-                            <Card.Img src={city} alt="cidade"/>
+                            <Card.Img src={city} alt="cidade" />
                         </Card.Body>
                     </Card>
                     <Card >
@@ -92,7 +128,7 @@ export default function Inicio() {
                             <Card.Title>População</Card.Title>
 
                             <Card.Text>{dados.estimated_population_2019}</Card.Text>
-                            <Card.Img src={population} alt="População"/>
+                            <Card.Img src={population} alt="População" />
                         </Card.Body>
                     </Card>
                     <Card >
@@ -100,7 +136,7 @@ export default function Inicio() {
                             <Card.Title>Casos confirmados</Card.Title>
 
                             <Card.Text>{dados.confirmed}</Card.Text>
-                            <Card.Img src={confirm} alt="Confirmados"/>
+                            <Card.Img src={confirm} alt="Confirmados" />
                         </Card.Body>
                     </Card>
                     <Card >
@@ -108,12 +144,12 @@ export default function Inicio() {
                             <Card.Title>Óbitos</Card.Title>
 
                             <Card.Text>{dados.deaths}</Card.Text>
-                            <Card.Img src={deaths} alt="Óbitos"/>
+                            <Card.Img src={deaths} alt="Óbitos" />
                         </Card.Body>
                     </Card>
                 </CardGroup>
                 <div >
-                    <img className="mapa-covid"  src={map} alt="Mapa"></img>
+                    <img className="mapa-covid" src={map} alt="Mapa"></img>
                 </div>
             </div>
         );
